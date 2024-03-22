8556bd587d9ba0a2a7842d4bac1b00580f8cc6aa
Xensen008 <arnabjyotikakati008@gmail.com>
2024-03-23 04:06:46 +0530

    sidebar added

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ const mongoose = require("mongoose");
 const Blog = require("./models/blog.models");
 const MongoStore = require("connect-mongo");
 const multer = require("multer")
-const {storage} = require("./cloudConfig");
-const upload = multer({storage});
+const { storage } = require("./cloudConfig");
+const upload = multer({ storage });
 
 const MONGO_URL = "mongodb+srv://arjun12345bhandari:D4igTLqjmffPe9oN@cluster3.iolho9r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3";
 
@@ -26,42 +26,42 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
-main().then(() =>{
-    console.log("DB connected");
-  }).catch((err) => {
-    console.log(err);
-  });
-  
-async function main(){
-    await mongoose.connect(MONGO_URL);
+main().then(() => {
+  console.log("DB connected");
+}).catch((err) => {
+  console.log(err);
+});
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
 const store = MongoStore.create({
-    mongoUrl: MONGO_URL,
-    crypto: {
-      secret: "qwertyuioplkjhgfdsazxcvbnm",
-    },
-    touchAfter: 24 * 3600,
-  });
+  mongoUrl: MONGO_URL,
+  crypto: {
+    secret: "qwertyuioplkjhgfdsazxcvbnm",
+  },
+  touchAfter: 24 * 3600,
+});
 
-  store.on("error", () => {
-    console.log("Error in MONGO SESSION STORE", err);
-  });
-  
+store.on("error", () => {
+  console.log("Error in MONGO SESSION STORE", err);
+});
 
-  const sessionOption = {
-    store,
-    secret: "asdefrelkoeurjvbbshdgterkcv",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true,
-    },
-  };
-  
-  app.use(session(sessionOption));
+
+const sessionOption = {
+  store,
+  secret: "asdefrelkoeurjvbbshdgterkcv",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOption));
 
 //   const newBlog = new Blog({
 //     images: [
@@ -92,21 +92,25 @@ const store = MongoStore.create({
 
 
 
-app.get('/',(req,res)=>{
-    res.render("pages/home.ejs");
+app.get('/', (req, res) => {
+  res.render("pages/home.ejs");
 })
 
-app.get('/about', (req,res)=>{
-res.render('pages/about.ejs')
+app.get('/about', (req, res) => {
+  res.render('pages/about.ejs')
 })
 
-app.get('/blog', async (req,res)=>{
+app.get('/sidebar', (req, res) => {
+  res.render('includes/sidebar.ejs')
+})
+
+app.get('/blog', async (req, res) => {
   const allBlog = await Blog.find({});
-res.render('pages/blog.ejs',{allBlog});
+  res.render('pages/blog.ejs', { allBlog });
 })
 
-app.post('/blog',upload.array('Blog[images][]', 6),async (req,res)=>{
-  try{
+app.post('/blog', upload.array('Blog[images][]', 6), async (req, res) => {
+  try {
 
     if (!req.files || req.files.length === 0) {
       return res.status(400).send('No files were uploaded.');
@@ -125,34 +129,34 @@ app.post('/blog',upload.array('Blog[images][]', 6),async (req,res)=>{
     const saveBlog = await newBlog.save();
     console.log(saveBlog);
     res.redirect("/blog");
-  }catch (error) {
+  } catch (error) {
     console.log(error.stack);
     res.status(500).send("Error uploading files");
   }
- 
+
 })
 
-app.get("/blog/form",(req,res)=>{
+app.get("/blog/form", (req, res) => {
   res.render("pages/createBlog.ejs");
 })
 
-app.get('/blog/:id',(req,res,)=>{
+app.get('/blog/:id', (req, res,) => {
   //Full Blog Will be seen here 
   let id = req.params;
   res.send(id);
 })
 
 
-app.get('/login', (req,res)=>{
-    res.send('Login Page')
+app.get('/login', (req, res) => {
+  res.send('Login Page')
 })
 
-app.get('/signup', (req,res)=>{
-    res.send('Signup page')
+app.get('/signup', (req, res) => {
+  res.send('Signup page')
 })
 
 
 
-app.listen(port, ()=>{
-    console.log(`Server Running at ${port}`);
+app.listen(port, () => {
+  console.log(`Server Running at ${port}`);
 })
